e6f18ca0f585b5c650cd946491d8222ef8e70627
minkolee <minkolee@gmail.com>
2018-11-13 14:47:06 +0800

    修改显示合同节点的部分

--- contracts/templatetags/contracts_tag.py
+++ contracts/templatetags/contracts_tag.py
@@ -12,3 +12,8 @@ def total_posts(decimal_obj):
 @register.filter(name='rate')
 def total_posts(decimal_obj):
     return "{:.2f}%".format(decimal_obj * Decimal('100'))
+
+
+@register.filter(name='show_as_para')
+def replace_return_with_br(str_obj):
+    return str_obj.replace('\r\n', '<br>').replace('script','p')
